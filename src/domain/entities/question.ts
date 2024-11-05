1fafe65a2e90e85dcea16bc3794d5b07566044a5
Matheus Martins <matheusms280100@gmail.com>
2024-11-05 19:53:55 -0300

    refactor Answer and Question entities; replace string IDs with UniqueEntityID value objects

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,15 @@
 import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entitiy"
+import { UniqueEntityID } from "../../core/entities/value-objects/unique-entity-id"
 
 interface QuestionProps {
+  authorId: UniqueEntityID
+  bestAnswerId?: UniqueEntityID
   title: string
   content: string
-  authorId: string
   slug: Slug
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Question extends Entity<QuestionProps> {}
